0538dbfeac17a40c815f6f1a91478af7a471fa28
hepiska <ego1403@gmail.com>
2020-05-22 00:13:09 +0700

    image base on schale

--- auto-schale-image/index.tsx
+++ auto-schale-image/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import { View, Image, ImageProps } from 'react-native';
 
 interface ImageType extends ImageProps {
@@ -7,8 +7,69 @@ interface ImageType extends ImageProps {
 }
 
 
-const ImageAutoScale = ({source, width, height,...props}: ImageType) => {
-      return <Image source={source} {...props} />
-}
+const ImageAutoScale = ({source, width,style, height,...props}: ImageType) => {
+
+  const [dim, setDim] = useState(null) as any
+  let isMounted = true
+    const calculateDimensions = (newWidth: number, newHeight: number) => {
+    if (width && height) {
+      return setDim({ width, height })
+    }
+    if (!width && !height) {
+      return setDim( { width: newWidth, height: newHeight } )
+    }
+    if (!height) {
+      const height = Number(newHeight)
+        ? Math.floor((width as number) * (newHeight / newWidth))
+        : width
+      return setDim({
+          width,
+          height,
+      })
+    }
+    if (!width) {
+      return setDim( { width: height * (newWidth / newHeight), height 
+      })
+    }
+  }
+
+  useEffect(() => {
+    // handle http source image
+      if((source as any).uri ){
+          Image.getSize((source as any).uri,(newWidth, newHeight) => {
+            //  stop calculation when component unmouted
+
+            if (!isMounted) {
+              return
+            }
+
+            calculateDimensions(newWidth, newHeight)
+          },(error) => {})
+      }else if(source){
+        // handle require local image
+        const { width: newWidth, height: newHeight } = Image.resolveAssetSource(
+          source,
+        )
+        if (!isMounted) {
+          return
+        }
+        calculateDimensions(newWidth, newHeight)
+      }
+      return () => {
+          isMounted = false
+      }
+  },[])
+  
+
+  console.log(dim)
+
+    if(dim) {
+      return <Image source={source} style={{ ...(style as object), height:dim.height, width:dim.width}} {...props} />
+
+    } 
+    return null
+  }
+
+      
 
 export default ImageAutoScale
